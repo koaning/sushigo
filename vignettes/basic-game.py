600428c6b9310dcda026f66cb1ba76bf442608d9
koaning <vincentwarmerdam@gmail.com>
2017-06-05 16:56:39 +0200

    vignette now shows off the gamelog

--- vignettes/basic-game.py
+++ vignettes/basic-game.py
@@ -3,4 +3,6 @@ import sushigo
 players = [sushigo.player.OrderedPlayer(), sushigo.player.Player()]
 game = sushigo.game.Game(players, verbose=True)
 game.play_full_game()
+
 print(game.scores)
+print(game.gamelog)

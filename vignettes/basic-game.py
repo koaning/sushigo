a9f68f98dd0c058fede581404e7e685ede8bf9a4
rob <romijndersrob@gmail.com>
2017-06-13 11:06:07 +0200

    winner in basic-game

--- vignettes/basic-game.py
+++ vignettes/basic-game.py
@@ -1,7 +1,12 @@
 import sushigo
 
-players = [sushigo.player.OrderedPlayer(), sushigo.player.Player()]
+p1 = sushigo.player.OrderedPlayer()
+p2 = sushigo.player.Player()
+
+players = [p1,p2]
 game = sushigo.game.Game(players, verbose=True)
-game.simulate_game()
+scores = game.play_full_game()
 print(game.scores)
 print(game.gamelog)
+
+print('Did player1 win? %s'%(game.calc_reward(p1.name)>game.calc_reward(p2.name)))

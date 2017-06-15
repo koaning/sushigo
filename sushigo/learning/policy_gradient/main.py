cd93e38d4e9df34fcb2689c544d252eeeb716372
rob <romijndersrob@gmail.com>
2017-06-15 08:58:17 +0200

    policy gradient trains one game

--- sushigo/learning/policy_gradient/main.py
+++ sushigo/learning/policy_gradient/main.py
@@ -2,33 +2,41 @@ import sushigo
 import numpy as np
 np.random.seed(123)
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
 from sushigo.deck import ALL_CARDTYPES
 from sushigo.learning.policy_gradient.players.simple_player import Simple_player
+from sushigo.learning.policy_gradient.players.pg_player import Pg_player
+from sushigo.learning.policy_gradient.policies.first_policy import Policy, select_action, finish_game
+from sushigo.game import Game
 
+#Set up policy
+policy = Policy()
+torch.manual_seed(123)
+
+#Parameters
 N_cards = len(ALL_CARDTYPES)
-
 trials = 10
 games_per_trial = 25
 max_weight = 100
+gamma = 0.99
+
+#Set up optim
+optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+log_interval = 10
 
 
-best_trial_points = -99999
-best_trial_weights = None
-for trial in range(trials):
-    trial_points = 0
+#Play one game
+p1 = Pg_player(policy=policy,name="PG_player01")
+p2 = Simple_player(weights=[26, 56, 54, 30, 77, 53, 53, 96, 78,  3, 77],name="SIMPLE_player01")
+game = Game([p1,p2],verbose=False)
+game.play_full_game()
+win = game.calc_reward(p1.name) > game.calc_reward(p2.name)
+print('Pg player did win: %s'%win)
 
-    trial_weights = np.random.randint(0,max_weight,size=(N_cards,))
-    for _ in range(games_per_trial):
-
-        p1 = Simple_player(weights=[26, 56, 54, 30, 77, 53, 53, 96, 78,  3, 77],name="ES_player02")
-        p2 = Simple_player(weights = trial_weights, name="ES_player01")
-
-        game = sushigo.game.Game([p1, p2], verbose=False)
-        scores = game.play_full_game()
-        trial_points += 2*int(game.calc_reward(p2.name)>game.calc_reward(p1.name))-1
-
-    if trial_points > best_trial_points:
-        best_trial_points = trial_points
-        best_trial_weights = trial_weights
-    print('Trial %5i best points %5i best weights %s'%(trial,best_trial_points, best_trial_weights))
-
+finish_game(p1.policy, gamma=gamma, optimizer=optimizer)
---
+++ sushigo/learning/policy_gradient/players/pg_player.py
@@ -0,0 +1,40 @@
+import uuid
+import random
+from sushigo.player import Player
+import numpy as np
+from sushigo.deck import ALL_CARDTYPES
+from collections import OrderedDict  #https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
+from sushigo.learning.policy_gradient.policies.first_policy import select_action
+
+NUM_CARDS = len(ALL_CARDTYPES)
+CARDS_IDX = {card:i for i,card in enumerate(ALL_CARDTYPES)}
+
+class Pg_player(Player):
+    def __init__(self, policy, name="simple_player"):
+        super(Pg_player, self).__init__()
+        self.policy = policy
+        self.name = name
+
+    def act(self, reward, observation=None, action_space=None):
+        self.policy.rewards.append(reward) #Append is one-off because action returns function
+        state = self.obs_to_state(observation)
+        allowed_actions = self.action_vec(action_space)
+        action = select_action(state=state,policy=self.policy,allowed=allowed_actions)
+        card = ALL_CARDTYPES[action]
+        return card
+
+    def obs_to_state(self,obs):
+        state_hand = [0]*NUM_CARDS
+        for card in obs['hand']:
+            state_hand[CARDS_IDX[card]] += 1
+        state_table = [0]*NUM_CARDS
+        for card in obs['table'][self.name]:
+            state_table[CARDS_IDX[card.type]] += 1
+        return state_hand + state_table
+
+    def action_vec(self, action_space):
+        actions = [0]*NUM_CARDS
+        for card in action_space:
+            actions[CARDS_IDX[card]] += 1
+        actions = list(map(lambda x: min(x,1), actions))
+        return actions

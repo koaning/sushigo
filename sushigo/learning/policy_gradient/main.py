6a649b4ab95ba3fca95359dd84af8ba90e8624f6
rob <romijndersrob@gmail.com>
2017-06-15 17:38:46 +0200

    polycy gradients training for 100 games

--- sushigo/learning/policy_gradient/main.py
+++ sushigo/learning/policy_gradient/main.py
@@ -1,13 +1,7 @@
-import sushigo
 import numpy as np
 np.random.seed(123)
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import torch.autograd as autograd
-from torch.autograd import Variable
 
 from sushigo.deck import ALL_CARDTYPES
 from sushigo.learning.policy_gradient.players.simple_player import Simple_player
@@ -21,9 +15,6 @@ torch.manual_seed(123)
 
 #Parameters
 N_cards = len(ALL_CARDTYPES)
-trials = 10
-games_per_trial = 25
-max_weight = 100
 gamma = 0.99
 
 #Set up optim
@@ -31,12 +22,17 @@ optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 log_interval = 10
 
 
-#Play one game
+#Play games
 p1 = Pg_player(policy=policy,name="PG_player01")
-p2 = Simple_player(weights=[26, 56, 54, 30, 77, 53, 53, 96, 78,  3, 77],name="SIMPLE_player01")
-game = Game([p1,p2],verbose=False)
-game.play_full_game()
-win = game.calc_reward(p1.name) > game.calc_reward(p2.name)
-print('Pg player did win: %s'%win)
+p2 = Simple_player(weights=[1/N_cards]*N_cards,name="SIMPLE_player01")
 
-finish_game(p1.policy, gamma=gamma, optimizer=optimizer)
+ewma = 0.5
+alpha = 0.95
+for n in range(100):
+    game = Game([p1,p2],verbose=False)
+    game.play_full_game()
+    win = game.calc_reward(p1.name) > game.calc_reward(p2.name)
+    ewma = alpha*ewma + (1-alpha)*int(win)
+    print('ewma win ratio %5.3f'%ewma)
+
+    finish_game(p1.policy, gamma=gamma, optimizer=optimizer)
